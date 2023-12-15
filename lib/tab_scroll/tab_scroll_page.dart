0877e900bd944285eb7f58223c4df2905d21f0ce
Railson Ferreira <railson.info@gmail.com>
2023-12-15 00:26:20 +0000

    refactor: Modify theme to set the preferred tabAlignment

--- lib/tab_scroll/tab_scroll_page.dart
+++ lib/tab_scroll/tab_scroll_page.dart
@@ -42,82 +42,89 @@ class _TabScrollPageState extends State<TabScrollPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Tab Scroll Page'),
       ),
-      body: ScrollableListTabScroller(
-        reverse: false,
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-        scrollDirection: Axis.vertical,
-        headerContainerBuilder: (context, child) {
-          return Container(
-            color: Colors.white,
-            child: child,
-          );
-        },
-        tabBuilder: (BuildContext context, int index, bool active) {
-          return Container(
-            width: size.width / 4,
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  color: active
-                      ? const Color.fromRGBO(0, 79, 224, 1)
-                      : Colors.transparent,
-                  width: 2,
-                ),
+      body: Theme(
+        data: Theme.of(context).copyWith(
+          tabBarTheme: Theme.of(context).tabBarTheme.copyWith(
+                tabAlignment: TabAlignment.start,
               ),
-            ),
-            child: Center(
-              child: Text(
-                "${resultList?[index].category}",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: active
-                      ? const Color.fromRGBO(0, 79, 224, 1)
-                      : Colors.black,
-                ),
-              ),
-            ),
-          );
-        },
-        itemCount: resultList!.length,
-        itemBuilder: (BuildContext context, int index) {
-          final data = resultList?[index];
-
-          return Column(
-            children: [
-              Column(
-                children: [
-                  Ink(
-                    color: Colors.lightGreen,
-                    child: ListTile(
-                      title: Text(
-                        '${data?.category}',
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                    ),
+        ),
+        child: ScrollableListTabScroller(
+          reverse: false,
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+          scrollDirection: Axis.vertical,
+          headerContainerBuilder: (context, child) {
+            return Container(
+              color: Colors.white,
+              child: child,
+            );
+          },
+          tabBuilder: (BuildContext context, int index, bool active) {
+            return Container(
+              width: size.width / 4,
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: active
+                        ? const Color.fromRGBO(0, 79, 224, 1)
+                        : Colors.transparent,
+                    width: 2,
                   ),
-                  for (int i = 0; i < data!.item!.length; i++)
-                    ListTile(
-                      title: Text(
-                        '${data.item?[i]}',
-                        style: const TextStyle(fontSize: 18),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  "${resultList?[index].category}",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: active
+                        ? const Color.fromRGBO(0, 79, 224, 1)
+                        : Colors.black,
+                  ),
+                ),
+              ),
+            );
+          },
+          itemCount: resultList!.length,
+          itemBuilder: (BuildContext context, int index) {
+            final data = resultList?[index];
+
+            return Column(
+              children: [
+                Column(
+                  children: [
+                    Ink(
+                      color: Colors.lightGreen,
+                      child: ListTile(
+                        title: Text(
+                          '${data?.category}',
+                          style: const TextStyle(fontSize: 18),
+                        ),
                       ),
                     ),
-                ],
-              ),
-              // data.category == category
-              //     ? Container(
-              //         height: size.height / 2,
-              //       )
-              //     : Container()
-            ],
-          );
-        },
-        tabChanged: (int tab) {
-          debugPrint("Tab ===> $tab");
-        },
-        initialAlignment: 100,
-        earlyChangePositionOffset: 50,
+                    for (int i = 0; i < data!.item!.length; i++)
+                      ListTile(
+                        title: Text(
+                          '${data.item?[i]}',
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      ),
+                  ],
+                ),
+                // data.category == category
+                //     ? Container(
+                //         height: size.height / 2,
+                //       )
+                //     : Container()
+              ],
+            );
+          },
+          tabChanged: (int tab) {
+            debugPrint("Tab ===> $tab");
+          },
+          initialAlignment: 100,
+          earlyChangePositionOffset: 50,
+        ),
       ),
     );
   }
